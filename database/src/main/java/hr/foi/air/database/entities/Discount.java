ab9a263d65ff9cbc50abe123cc0f5f54c9417990
Zlatko S <zlatko.stapic@foi.hr>
2020-10-29 07:10:43 +0100

    03 Add Store and Discount entity classes  - create new package: entities  - add classes and attributes (according to your data model)  - use Generate Getters and Setters option

---
+++ database/src/main/java/hr/foi/air/database/entities/Discount.java
@@ -0,0 +1,69 @@
+package hr.foi.air.database.entities;
+
+import java.util.Date;
+
+public class Discount {
+    int id;
+    int storeId;
+    String name;
+    String description;
+    int discountValue;
+    Date startDate;
+    Date endDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(int storeId) {
+        this.storeId = storeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDiscountValue() {
+        return discountValue;
+    }
+
+    public void setDiscountValue(int discountValue) {
+        this.discountValue = discountValue;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+}
---
+++ database/src/main/java/hr/foi/air/database/entities/Store.java
@@ -0,0 +1,58 @@
+package hr.foi.air.database.entities;
+
+public class Store {
+    int id;
+    String name;
+    String description;
+    String imgUrl;
+    long longitude;
+    long latitude;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public long getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(long longitude) {
+        this.longitude = longitude;
+    }
+
+    public long getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(long latitude) {
+        this.latitude = latitude;
+    }
+}
