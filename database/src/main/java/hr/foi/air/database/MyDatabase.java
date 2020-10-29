c0615ada6d763be5e883f57a71fa8d1ad8e310ee
Zlatko S <zlatko.stapic@foi.hr>
2020-10-29 07:28:23 +0100

    03 Make room database accessible  - use singleton pattern to make a database as a single instance

--- database/src/main/java/hr/foi/air/database/MyDatabase.java
+++ database/src/main/java/hr/foi/air/database/MyDatabase.java
@@ -1,6 +1,9 @@
 package hr.foi.air.database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import hr.foi.air.database.entities.Discount;
@@ -10,4 +13,19 @@ import hr.foi.air.database.views.DiscountDetails;
 @Database(version = 1, entities = {Store.class, Discount.class},
         views = {DiscountDetails.class}, exportSchema = false)
 public abstract class MyDatabase extends RoomDatabase {
+    public static final String NAME = "main";
+    public static final int VERSION = 1;
+
+    private static MyDatabase INSTANCE = null;
+
+    public synchronized static MyDatabase getInstance(final Context context) {
+        if(INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    MyDatabase.class,
+                    MyDatabase.NAME
+            ).allowMainThreadQueries().build();
+        }
+        return INSTANCE;
+    }
 }
