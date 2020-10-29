7b0944d2d8c2cd6c7eda412c9e4f39962fd27001
Zlatko S <zlatko.stapic@foi.hr>
2020-10-29 07:35:01 +0100

    03 Use database in your activity  - add room reference to app module  - use static import to add reference to database (which is abstract class)  - Challenge: think of redesigning the app to avoid double room dependencies (in database and in app module)

--- app/src/main/java/hr/foi/air/discountlocator/MainActivity.java
+++ app/src/main/java/hr/foi/air/discountlocator/MainActivity.java
@@ -7,16 +7,23 @@ import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import hr.foi.air.database.MyDatabase;
+
+import static hr.foi.air.database.MyDatabase.getInstance;
 
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.txt_hello)
     TextView txtHello;
 
+    public static MyDatabase database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        database = getInstance(this);
 
         ButterKnife.bind(this);
 
