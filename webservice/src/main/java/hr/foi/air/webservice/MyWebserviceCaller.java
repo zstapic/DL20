970a5d1eecc1967dc12a05c9bafbec6feb9e3407
Zlatko S <zlatko.stapic@foi.hr>
2020-11-12 05:10:03 +0100

    04 Web service caller  - implement constructor  - define baseUrl, http client and retrofit object  - add getAll method stub

--- webservice/src/main/java/hr/foi/air/webservice/MyWebserviceCaller.java
+++ webservice/src/main/java/hr/foi/air/webservice/MyWebserviceCaller.java
@@ -1,4 +1,38 @@
 package hr.foi.air.webservice;
 
+import com.squareup.okhttp.OkHttpClient;
+
+import java.lang.reflect.Type;
+
+import retrofit.GsonConverterFactory;
+import retrofit.Retrofit;
+
 public class MyWebserviceCaller {
+    // retrofit object
+    Retrofit retrofit;
+    // base URL of the web service
+    private final String baseUrl = "http://cortex.foi.hr/mtl/courses/air/";
+
+    // constructor
+    public MyWebserviceCaller(){
+
+        OkHttpClient client = new OkHttpClient();
+
+        // for debuggint use HttpInterceptor
+        //client.interceptors().add(new HttpInterceptor());
+
+        this.retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+    }
+
+    // get all records from a web service
+    public void getAll(String method, final Type entityType){
+
+        //todo: implement web service call and response handling
+
+    }
 }
