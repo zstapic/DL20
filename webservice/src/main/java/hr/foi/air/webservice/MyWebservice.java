72d428a1363acc17b24a5d3f2e7bc253643c1fc9
Zlatko S <zlatko.stapic@foi.hr>
2020-11-12 05:06:45 +0100

    04 Define MyWebservice interface  - this interface is used to represent a gateway to web API  - it should declare methods that will be used in actual calls towards web service  - these methods, as they are anotated here, will be implemented by retrofit

--- webservice/src/main/java/hr/foi/air/webservice/MyWebservice.java
+++ webservice/src/main/java/hr/foi/air/webservice/MyWebservice.java
@@ -1,4 +1,17 @@
 package hr.foi.air.webservice;
 
+import hr.foi.air.webservice.responses.MyWebserviceResponse;
+import retrofit.Call;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.POST;
+
 public interface MyWebservice {
+    @FormUrlEncoded
+    @POST("stores.php")
+    Call<MyWebserviceResponse> getStores(@Field("method") String method);
+
+    @FormUrlEncoded
+    @POST("discounts.php")
+    Call<MyWebserviceResponse> getDiscounts(@Field("method") String method);
 }
