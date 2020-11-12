16f5e213e2a27f73fcbb834b813427ce06ea27a5
Zlatko S <zlatko.stapic@foi.hr>
2020-11-12 04:56:38 +0100

    04 Define response  - use web service specification document and/or Postman tool (https://www.postman.com/) to examine and test web services  - define MyWebservice class atributes  - add getters and setters

--- webservice/src/main/java/hr/foi/air/webservice/responses/MyWebserviceResponse.java
+++ webservice/src/main/java/hr/foi/air/webservice/responses/MyWebserviceResponse.java
@@ -1,4 +1,40 @@
 package hr.foi.air.webservice.responses;
 
 public class MyWebserviceResponse {
+    public int responseId;
+    public String responseText;
+    public long timeStamp;
+    public String items;
+
+    public int getResponseId() {
+        return responseId;
+    }
+
+    public void setResponseId(int responseId) {
+        this.responseId = responseId;
+    }
+
+    public String getResponseText() {
+        return responseText;
+    }
+
+    public void setResponseText(String responseText) {
+        this.responseText = responseText;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getItems() {
+        return items;
+    }
+
+    public void setItems(String items) {
+        this.items = items;
+    }
 }
