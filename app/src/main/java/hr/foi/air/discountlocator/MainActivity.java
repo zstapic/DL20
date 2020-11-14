6e4360db88750874cddac2112b3ea425a84f7b3f
Zlatko S <zstapic@foi.hr>
2020-11-14 13:59:55 +0100

    05 House keeping  - remove code we do not need any more  - did we contribute to Single responsibility principle in MainActivity?

--- app/src/main/java/hr/foi/air/discountlocator/MainActivity.java
+++ app/src/main/java/hr/foi/air/discountlocator/MainActivity.java
@@ -3,12 +3,9 @@ package hr.foi.air.discountlocator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,31 +15,22 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import hr.foi.air.core.DataLoadedListener;
 import hr.foi.air.core.DataLoader;
-import hr.foi.air.database.MyDatabase;
-import hr.foi.air.database.data.MockData;
 import hr.foi.air.database.entities.Discount;
 import hr.foi.air.database.entities.Store;
 import hr.foi.air.discountlocator.loaders.DbDataLoader;
 import hr.foi.air.discountlocator.loaders.WsDataLoader;
-
-import static hr.foi.air.database.MyDatabase.getInstance;
 
 public class MainActivity extends AppCompatActivity implements DataLoadedListener {
 
     @BindView(R.id.discount_list)
     ListView mListView;
 
-    public static MyDatabase database;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        database = getInstance(this);
-
         ButterKnife.bind(this);
-        mockData();
     }
 
     @OnClick(R.id.test_button)
@@ -50,25 +38,6 @@ public class MainActivity extends AppCompatActivity implements DataLoadedListene
     {
         DataLoader dataLoader = new WsDataLoader();
         dataLoader.loadData(this);
-    }
-
-
-    private void mockData()
-    {
-        List<Store> stores = database.getDAO().loadAllStores();
-        if (!stores.isEmpty()) {
-            for (Store s : stores) {
-                Log.d("AIRAIR","Store: " + s.getName());
-                List<Discount> discounts
-                        = database.getDAO().loadAllDiscountsByStore(s.getId());
-                for (Discount d : discounts) {
-                    Log.d("AIRAIR","Discount: " + d.getName());
-                    //System.out.println("Discount: " + d.getName());
-                }
-            }
-        }else {
-            MockData.writeAll(this);
-        }
     }
 
     @Override
