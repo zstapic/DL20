88b054b25a4b129aa0d0cb05707df6a829683b4f
Zlatko S <zstapic@foi.hr>
2020-11-21 10:19:59 +0100

    06 [RecyclerView] Create StoreViewHolder  - create new class that extends ParentViewHolder  - add default constructor  - define and bind view elements  - define bind method  - use Picasso to load image, dependency to library is added in the first commit

---
+++ app/src/main/java/hr/foi/air/discountlocator/recyclerview/StoreViewHolder.java
@@ -0,0 +1,44 @@
+package hr.foi.air.discountlocator.recyclerview;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.bignerdranch.expandablerecyclerview.ParentViewHolder;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import hr.foi.air.database.entities.Store;
+import hr.foi.air.discountlocator.R;
+
+public class StoreViewHolder extends ParentViewHolder {
+
+    @BindView(R.id.store_name)
+    TextView storeName;
+    @BindView(R.id.store_desc)
+    TextView storeDesc;
+    @BindView(R.id.store_image)
+    ImageView storeImage;
+
+    /**
+     * Default constructor.
+     *
+     * @param itemView The {@link View} being hosted in this ViewHolder
+     */
+    public StoreViewHolder(@NonNull View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+    }
+
+    public void bindToData(Store store)
+    {
+        storeName.setText(store.getName());
+        storeDesc.setText(store.getDescription());
+        Picasso.get()
+                .load(store.getImgUrl())
+                .into(storeImage);
+    }
+}
