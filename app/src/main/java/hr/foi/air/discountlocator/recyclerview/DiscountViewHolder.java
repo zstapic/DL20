03e0d91f358f9798d57a6a838a6f4d2e43b2dbd1
Zlatko S <zstapic@foi.hr>
2020-11-21 10:24:52 +0100

    06 [RecyclerView] Create DiscountViewHolder  - create new class that extends ChildViewHolder  - add default constructor  - define and bind view elements  - define bind method

---
+++ app/src/main/java/hr/foi/air/discountlocator/recyclerview/DiscountViewHolder.java
@@ -0,0 +1,40 @@
+package hr.foi.air.discountlocator.recyclerview;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.bignerdranch.expandablerecyclerview.ChildViewHolder;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import hr.foi.air.database.entities.Discount;
+import hr.foi.air.discountlocator.R;
+
+public class DiscountViewHolder extends ChildViewHolder<Discount> {
+
+    @BindView(R.id.discount_name)
+    TextView discountName;
+    @BindView(R.id.discount_desc)
+    TextView discountDesc;
+    @BindView(R.id.discount_value)
+    TextView discountValue;
+
+    /**
+     * Default constructor.
+     *
+     * @param itemView The {@link View} being hosted in this ViewHolder
+     */
+    public DiscountViewHolder(@NonNull View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+    }
+
+    public void bindToData(Discount discount)
+    {
+        discountName.setText(discount.getName());
+        discountDesc.setText(discount.getDescription());
+        discountValue.setText(discount.getDiscountValue() + "%");
+    }
+}
