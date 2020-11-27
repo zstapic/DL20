eac353bedd3131d396350ab6f28d15782fa8c632
Zlatko S <zstapic@foi.hr>
2020-11-27 13:45:05 +0100

    08 [Fragments] Display fragment dynamically  - Add FrameLayout in activity's layout  - Add code to display fragment using Fragment Transaction object

--- app/src/main/java/hr/foi/air/discountlocator/MainActivity.java
+++ app/src/main/java/hr/foi/air/discountlocator/MainActivity.java
@@ -1,7 +1,11 @@
 package hr.foi.air.discountlocator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
+
+import hr.foi.air.discountlocator.fragments.DiscountListFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayMainFragment();
+    }
+
+    private void displayMainFragment()
+    {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment_container, new DiscountListFragment());
+        ft.commit();
     }
 }
