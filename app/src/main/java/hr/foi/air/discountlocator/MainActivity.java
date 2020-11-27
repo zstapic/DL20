82ff09b22bb8b1a51c671e8357074e444bb7f46d
Zlatko S <zstapic@foi.hr>
2020-11-27 20:30:05 +0100

    09 [Navigation Drawer] Put it all together  - Update main activity to set our toolbar as actionbar with  - Update main activity define navigationUI controller with navigation drawer  - Handle navigation icon menu and up behaviour with onSupportNavigateUp event

--- app/src/main/java/hr/foi/air/discountlocator/MainActivity.java
+++ app/src/main/java/hr/foi/air/discountlocator/MainActivity.java
@@ -1,13 +1,26 @@
 package hr.foi.air.discountlocator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.navigation.NavigationView;
 
 import hr.foi.air.discountlocator.fragments.DiscountListFragment;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    private AppBarConfiguration mAppBarConfiguration;
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +32,26 @@ public class MainActivity extends AppCompatActivity {
 
     private void displayMainFragment()
     {
-        //FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        //ft.replace(R.id.fragment_container, new DiscountListFragment());
-        //ft.commit();
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.drawer_nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_disocunt_list)
+                .setDrawerLayout(drawer)
+                .build();
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
